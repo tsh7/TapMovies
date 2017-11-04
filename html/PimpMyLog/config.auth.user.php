751914badfa05a2395522c47b541fe0bf09405a0
tsh7 <32529092+tsh7@users.noreply.github.com>
2017-11-04 17:15:03 -0400

    Add files via upload

---
+++ html/PimpMyLog/config.auth.user.php
@@ -0,0 +1,2 @@
+<?php if (realpath(__FILE__)===realpath($_SERVER["SCRIPT_FILENAME"])) {header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");die();}?>
+{"generated":"1509263806","security":"Xq5S8Ze9l8WrRqErAR-N3fT12dAv7ikKQj6xu1-PF-A0nlVyNVZ04a9KW0kasqaW","anonymous":[],"users":{"admin":{"roles":["admin"],"pwd":"80bda48c9804862d0254c52164f0448ce4c63182","logs":[],"cd":"1509263815","cb":null,"at":"DKAmbXxmzZKk7rupY4Nopmu0fXUNwxus","hp":"2q-w_9FddLyKrfCj"}}}
