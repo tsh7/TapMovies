b57798d1ebf7878b7f881851ad49ab0556b84b90
Mangab <canon825@gmail.com>
2017-11-09 09:43:39 -0500

    Update TopRate_WWW_T_API.php

--- DB/WWW_Part/TopRate_WWW_T_API.php
+++ DB/WWW_Part/TopRate_WWW_T_API.php
@@ -1,20 +1,21 @@
 <?php
-	$TopRate= array("Function"=>"TopRate");
+// This is requesting Toprate movies from WWW. Webserver requests top rated movie info to API
+	$TopRate= array("Function"=>"TopRate");//only one key and value is used
 
-	$data = json_encode($TopRate);
+	$data = json_encode($TopRate);// convert php array, $TopRate, to json data
 
-	require_once __DIR__ . '/vendor/autoload.php';
-	use PhpAmqpLib\Connection\AMQPConnection;
-	use PhpAmqpLib\Message\AMQPMessage;
+	require_once __DIR__ . '/vendor/autoload.php';//RabbitMQ library
+	use PhpAmqpLib\Connection\AMQPConnection;//RabbitMQ library
+	use PhpAmqpLib\Message\AMQPMessage;//RabbitMQ library
 
-	$connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');
-	$channel = $connection->channel();
+	$connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip and rabbitMQ account if needed
+	$channel = $connection->channel();//create channel
 
-	$channel->queue_declare('WWW_T_API', false, false, false, false);
+	$channel->queue_declare('WWW_T_API', false, false, false, false);//open channel. Start of listening
 
-	$msg = new AMQPMessage($data, array('delivery_mode' => 2));
-	$channel->basic_publish($msg, '', 'WWW_T_API');
+	$msg = new AMQPMessage($data, array('delivery_mode' => 2));//convert json data to rabbitMQ message
+	$channel->basic_publish($msg, '', 'WWW_T_API');//send message to API
 	$channel->close();
 	$connection->close();
-	include 'API_T_WWW.php';
+	include 'API_T_WWW.php'; //After sending request for TopRate, webserver needs to receive the result. API_T_WWW.php is receving part of webserver from API
 ?>
