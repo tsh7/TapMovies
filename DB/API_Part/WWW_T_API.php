4f8e71d0902e5e68cc8c81750487204c044bc511
Mangab <canon825@gmail.com>
2017-11-09 10:58:44 -0500

    add comment
    add comment

--- DB/API_Part/WWW_T_API.php
+++ DB/API_Part/WWW_T_API.php
@@ -1,37 +1,39 @@
     <?php
-    require_once __DIR__ . '/vendor/autoload.php';
-    use PhpAmqpLib\Connection\AMQPStreamConnection;
+//This is receiving part. It continuously listening to the Webserver
 
-    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');
-    $channel = $connection->channel();
+    require_once __DIR__ . '/vendor/autoload.php';//RabbitMQ library
+    use PhpAmqpLib\Connection\AMQPStreamConnection;//RabbitMQ library
 
-    $channel->queue_declare('WWW_T_API', false, false, false, false);
+    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip and rabbitmq account info if needed
+    $channel = $connection->channel();//create channel
 
-    echo ' * Waiting for messages. To exit press CTRL+C', "\n";
+    $channel->queue_declare('WWW_T_API', false, false, false, false);//open channel
 
-    $callback = function($msg){
+    echo ' * Waiting for messages. To exit press CTRL+C', "\n";//display if it is listening to
+
+    $callback = function($msg){//Display if message received from Webserver
 
         echo " * Message received", "\n";
 
-    $data = json_decode($msg->body, true);
+    $data = json_decode($msg->body, true);//convert json data to php array
 
-    $Function = $data["Function"];
+    $Function = $data["Function"];//Store value of the key, Function
 
     switch ($Function){
-	case "Search_movie":
+	case "Search_movie"://if Function value is "Search_movie", do Search.php
 		include 'Search.php';
 		break;
-	case "TopRate":
+	case "TopRate"://if Function value is "TopRate", do TopRate.php
 		include 'TopRate.php';
 		break;
-	default:
-		echo "unknown error";
+	default://if data received from Webserver is something wrong, display error message
+		echo "Key or value is something wrong";
     }
 
 
     };
     $channel->basic_consume('WWW_T_API', '', false, true, false, false, $callback);
-    while(count($channel->callbacks)) {
+    while(count($channel->callbacks)) {//keep listen to the rabbitMQ
             $channel->wait();
     }
     $channel->close();
