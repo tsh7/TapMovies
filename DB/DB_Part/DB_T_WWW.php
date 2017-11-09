1cc7cf2a670e445bc56001c42508b817b559c508
Mangab <canon825@gmail.com>
2017-11-09 09:53:29 -0500

    add commnet
    add commnet

--- DB/DB_Part/DB_T_WWW.php
+++ DB/DB_Part/DB_T_WWW.php
@@ -1,18 +1,19 @@
 <?php
-    $r_data = json_encode($R_Data);
+//This is sending part of DB. It returns after processing the work to the Webserver
+    $r_data = json_encode($R_Data);//receive $R_Data from other files after processing tasks, and convert it to json data
 
-    require_once __DIR__ . '/vendor/autoload.php';
-    use PhpAmqpLib\Connection\AMQPStreamConnection;
-    use PhpAmqpLib\Message\AMQPMessage;
+    require_once __DIR__ . '/vendor/autoload.php';//RabbitMQ library
+    use PhpAmqpLib\Connection\AMQPStreamConnection;//RabbitMQ library
+    use PhpAmqpLib\Message\AMQPMessage;//RabbitMQ library
 
-    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');
-    $channel = $connection->channel();
+    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip and rabbitMQ account info if needed
+    $channel = $connection->channel();//create channel
 
-    $channel->queue_declare('DB_T_WWW', false, false, false, false);
+    $channel->queue_declare('DB_T_WWW', false, false, false, false);//open channel
 
-    $msg = new AMQPMessage($r_data, array('delivery_mode' => 2));
-    $channel->basic_publish($msg, '', 'DB_T_WWW');
+    $msg = new AMQPMessage($r_data, array('delivery_mode' => 2));//convert json data to rabbitMQ message
+    $channel->basic_publish($msg, '', 'DB_T_WWW');//send message to webserver
     $channel->close();
     $connection->close();
-print_r($R_Data);
+print_r($R_Data);//print after successfully finished its task
 ?>
