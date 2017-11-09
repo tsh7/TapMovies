58846de63659908f92dd456660126fdbd8f57729
Mangab <canon825@gmail.com>
2017-11-09 10:26:23 -0500

    add commnet
    add commnet

--- DB/API_Part/API_T_WWW.php
+++ DB/API_Part/API_T_WWW.php
@@ -1,18 +1,19 @@
 <?php
-    $r_data = json_encode($php_data);
+//This is sending part of API. After processing tasks, it returns the result to the Webserver
+    $r_data = json_encode($php_data);//convert $php_data array to json data
 
-    require_once __DIR__ . '/vendor/autoload.php';
-    use PhpAmqpLib\Connection\AMQPStreamConnection;
-    use PhpAmqpLib\Message\AMQPMessage;
+    require_once __DIR__ . '/vendor/autoload.php';//ribbitMQ library
+    use PhpAmqpLib\Connection\AMQPStreamConnection;//ribbitMQ library
+    use PhpAmqpLib\Message\AMQPMessage;//ribbitMQ library
 
-    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');
-    $channel = $connection->channel();
+    $connection = new AMQPStreamConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip and rabbitMQ account info if needed
+    $channel = $connection->channel();//create channel
 
-    $channel->queue_declare('API_T_WWW', false, false, false, false);
+    $channel->queue_declare('API_T_WWW', false, false, false, false);//open channel
 
-    $msg = new AMQPMessage($r_data, array('delivery_mode' => 2));
-    $channel->basic_publish($msg, '', 'API_T_WWW');
+    $msg = new AMQPMessage($r_data, array('delivery_mode' => 2));//convert json data to rabbitMQ message
+    $channel->basic_publish($msg, '', 'API_T_WWW');//send message to Webserver
     $channel->close();
     $connection->close();
-//print_r($r_data);
+//print_r($r_data);//test purpose to see if the result is good or not
 ?>
