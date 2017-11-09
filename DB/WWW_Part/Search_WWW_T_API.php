2b22c4e5385af7721e192040a9517f3a82f15b32
Mangab <canon825@gmail.com>
2017-11-09 09:37:47 -0500

    add comment
    add comment

--- DB/WWW_Part/Search_WWW_T_API.php
+++ DB/WWW_Part/Search_WWW_T_API.php
@@ -1,21 +1,22 @@
 <?php
-	$M_name = "suburbicon";
-	$Search = array("Function"=>"Search_movie","M_name" => $M_name);
+//This is search part of WWW. Webserver sends movie name to API.
+	$M_name = "suburbicon";//movie name
+	$Search = array("Function"=>"Search_movie","M_name" => $M_name);//create php array with movie name to send
 
-	$data = json_encode($Search);
+	$data = json_encode($Search);// convert php array to json data
 
-	require_once __DIR__ . '/vendor/autoload.php';
-	use PhpAmqpLib\Connection\AMQPConnection;
-	use PhpAmqpLib\Message\AMQPMessage;
+	require_once __DIR__ . '/vendor/autoload.php';//RabbitMQ library
+	use PhpAmqpLib\Connection\AMQPConnection;//RabbitMQ library
+	use PhpAmqpLib\Message\AMQPMessage;//RabbitMQ library
 
-	$connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');
-	$channel = $connection->channel();
+	$connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip and RabbitMQ account info if needed
+	$channel = $connection->channel();//create channel
 
-	$channel->queue_declare('WWW_T_API', false, false, false, false);
+	$channel->queue_declare('WWW_T_API', false, false, false, false);//open channel
 
-	$msg = new AMQPMessage($data, array('delivery_mode' => 2));
-	$channel->basic_publish($msg, '', 'WWW_T_API');
+	$msg = new AMQPMessage($data, array('delivery_mode' => 2));//convert json array to rabbitMQ message type
+	$channel->basic_publish($msg, '', 'WWW_T_API');//send message
 	$channel->close();
 	$connection->close();
-	include 'API_T_WWW.php';
+	include 'API_T_WWW.php';// After sending, WWW need to receive the search result. API_T_WWW.php is receving part
 ?>
