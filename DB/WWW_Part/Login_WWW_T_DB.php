28af0e96274c46e97dc213277ce8a300b2589072
Mangab <canon825@gmail.com>
2017-11-09 09:24:53 -0500

    add comment
    add comment

--- DB/WWW_Part/Login_WWW_T_DB.php
+++ DB/WWW_Part/Login_WWW_T_DB.php
@@ -1,23 +1,25 @@
     <?php
-    $Pword_text = 'mk612';
-    $Hash_Pword = crypt($Pword_text, 'ABCDE12345');
+//This is sending part of WWW for user login. The user info goes to DB server to login
 
-    $U_inf= array("Function"=>"Login","Email"=>"mk612@njit.edu","Pword"=>$Hash_Pword);
+    $Pword_text = 'mk612'; //user password 
+    $Hash_Pword = crypt($Pword_text, 'ABCDE12345'); //hashing user password
 
-    $data = json_encode($U_inf);
+    $U_inf= array("Function"=>"Login","Email"=>"mk612@njit.edu","Pword"=>$Hash_Pword);//array: user login info: email and password
 
-    require_once __DIR__ . '/vendor/autoload.php';
-    use PhpAmqpLib\Connection\AMQPConnection;
-    use PhpAmqpLib\Message\AMQPMessage;
+    $data = json_encode($U_inf);//encode as json array
 
-    $connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');
+    require_once __DIR__ . '/vendor/autoload.php';//use rabbitMQ library
+    use PhpAmqpLib\Connection\AMQPConnection;//use rabbitMQ library
+    use PhpAmqpLib\Message\AMQPMessage;//use rabbitMQ library
+
+    $connection = new AMQPConnection('10.0.2.4', 5672, 'admin', 'guest');//change ip address and rabbitMQ account info
     $channel = $connection->channel();
 
-    $channel->queue_declare('WWW_T_DB', false, false, false, false);
+    $channel->queue_declare('WWW_T_DB', false, false, false, false);//queue name
 
     $msg = new AMQPMessage($data, array('delivery_mode' => 2));
     $channel->basic_publish($msg, '', 'WWW_T_DB');
     $channel->close();
     $connection->close();
-    include 'DB_T_WWW.php';
+    include 'DB_T_WWW.php'; // After sending the data, Web server need to listen to the DB server for the result. DB_T_WWW.php is receiving part
    ?>
