b154c76aaf9e90132cc726760dde1c94e6e4d99c
kuldeep212 <33297628+kuldeep212@users.noreply.github.com>
2017-11-29 16:45:10 -0500

    Create API_T_WWW.php

---
+++ API/API_T_WWW.php
@@ -0,0 +1,28 @@
+<?php
+//Seding file to Web Server. 
+
+    //convert php array to json data
+    $r_data = json_encode($php_data);
+    
+    //rabbbitMQ library
+    require_once __DIR__ . '/vendor/autoload.php';
+    use PhpAmqpLib\Connection\AMQPStreamConnection;
+    use PhpAmqpLib\Message\AMQPMessage;
+    
+    //change ip and rabbitMQ account info if needed
+    $connection = new AMQPStreamConnection('10.0.2.15', 5672, 'admin', 'admin');
+    
+    //create channel
+    $channel = $connection->channel();
+    
+    //open channel
+    $channel->queue_declare('API_T_WWW', true, false, false, false);
+    
+    //convert json data to rabbitMQ message
+    $msg = new AMQPMessage($r_data, array('delivery_mode' => 2));
+    
+    //send message to Webserver
+    $channel->basic_publish($msg, '', 'API_T_WWW');
+    $channel->close();
+    $connection->close();
+?>
