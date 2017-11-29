07baac087c755e63044c5f8b6d4283d1f65f9fc6
kuldeep212 <33297628+kuldeep212@users.noreply.github.com>
2017-11-29 17:23:43 -0500

    Create TopRate.php

---
+++ API/TopRate.php
@@ -0,0 +1,19 @@
+<?php
+//Api request to tmdb request for top rate movies 
+
+//address of toprate in TMDB
+$url = 'https://api.themoviedb.org/3/discover/movie?api_key=4d9ca8e7d8e3381c21ffeb6f1306ee32&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1';
+
+//store the result to $json_data.
+$json_data = file_get_contents($url);
+
+//convert json data to php array
+$php_data = json_decode($json_data,true);
+
+//Return data to Webserever
+include 'API_T_WWW.php';//Return data to Webserever
+
+//find a specific value in an array inside of another array
+print_r($php_contents["results"][0]["100"]); 
+print_r(count($php_contents["results"]));
+?>
