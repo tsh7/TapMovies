1ad3cc4cb3134d83f5662f77998cb17fd4cbc0f9
kuldeep212 <33297628+kuldeep212@users.noreply.github.com>
2017-11-29 17:18:38 -0500

    Create Search_WWW_T_API.php

---
+++ API/Search_WWW_T_API.php
@@ -0,0 +1,39 @@
+<?php
+//Webserver sends to API (search)
+  
+  //movie name
+	$M_name = "golmaal";
+  
+  //create php array with movie name to send
+	$Search = array("Function"=>"Search_movie","M_name" => $M_name);
+	
+  // convert php array to json data
+  $data = json_encode($Search);
+  
+  //RabbitMQ library
+	require_once __DIR__ . '/vendor/autoload.php';
+	use PhpAmqpLib\Connection\AMQPConnection;
+	use PhpAmqpLib\Message\AMQPMessage;
+  
+  //change ip and RabbitMQ account info if needed
+	$connection = new AMQPConnection('10.0.2.15', 5672, 'admin', 'admin');
+  
+  //create channel
+	$channel = $connection->channel();
+  
+  //open channel
+	$channel->queue_declare('WWW_T_API', true, false, false, false);
+  
+  //convert json array to rabbitMQ message type
+	$msg = new AMQPMessage($data, array('delivery_mode' => 2));
+  
+  //send message
+	$channel->basic_publish($msg, '', 'WWW_T_API');
+	$channel->close();
+	$connection->close();
+  
+  // After sending, WWW need to receive the search result. API_T_WWW.php is receving part
+	include 'Webserver_API_T_WWW.php';
+  
+  ?>
+  
