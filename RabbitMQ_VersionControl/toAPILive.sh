f63ca142d2371a221167e7176e37fdad52501dfd
zethrath132 <bl233@njit.edu>
2017-12-20 18:13:17 -0500

    Create toAPILive.sh

---
+++ RabbitMQ_VersionControl/toAPILive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#sends the named APIServer Version to the Live APIServer
+        scp ben@192.168.1.101:/home/ben/IT490/VersionControl/APIVersion/$1 kuldeep@192.168.1.113:/home/kuldeep
