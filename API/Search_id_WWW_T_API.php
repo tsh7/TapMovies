aa9d1f95cf63188e44115ec4ad4280c90eb6c13c
kuldeep212 <33297628+kuldeep212@users.noreply.github.com>
2017-12-20 12:43:23 -0500

    Update Search_id_WWW_T_API.php

--- API/Search_id_WWW_T_API.php
+++ API/Search_id_WWW_T_API.php
@@ -1,17 +1,39 @@
 <?php
-//This is search part of WWW. Webserver sends movie name to API.
-	$M_id = "54622";//movie name
-	$Search = array("Function"=>"Search_movie_id","M_id" => $M_id);//create php array with movie name to send
-	$data = json_encode($Search);// convert php array to json data
-	require_once __DIR__ . '/vendor/autoload.php';//RabbitMQ library
-	use PhpAmqpLib\Connection\AMQPConnection;//RabbitMQ library
-	use PhpAmqpLib\Message\AMQPMessage;//RabbitMQ library
-	$connection = new AMQPConnection('192.168.1.101', 5672, 'admin', 'guest');//change ip and RabbitMQ account info if needed
-	$channel = $connection->channel();//create channel
-	$channel->queue_declare('WWW_T_API', false, false, false, false);//open channel
-	$msg = new AMQPMessage($data, array('delivery_mode' => 2));//convert json array to rabbitMQ message type
-	$channel->basic_publish($msg, '', 'WWW_T_API');//send message
+//WWW sends request to API 
+
+	//movie id 
+	$M_id = "54622";
+
+	//php array with movie id 
+	$Search = array("Function"=>"Search_movie_id","M_id" => $M_id);
+	
+	// convert php to json
+	$data = json_encode($Search);
+	
+	//RabbitMQ libraries 
+	require_once __DIR__ . '/vendor/autoload.php';
+	use PhpAmqpLib\Connection\AMQPConnection;
+	use PhpAmqpLib\Message\AMQPMessage;
+	
+	//RabbitMQ account information 
+	$connection = new AMQPConnection('192.168.1.101', 5672, 'admin', 'guest');
+	
+	//create new channels 
+	$channel = $connection->channel();
+	
+	//open channels 
+	$channel->queue_declare('WWW_T_API', false, false, false, false);
+	
+	//convert json data to rabbitMQ message to communicate 
+	$msg = new AMQPMessage($data, array('delivery_mode' => 2));
+
+	//send message to API 
+	$channel->basic_publish($msg, '', 'WWW_T_API');
+
+	//close channel and connection 
 	$channel->close();
 	$connection->close();
-	include 'Webserver_API_T_WWW.php';// After sending, WWW need to receive the search result. API_T_WWW.php is receving part
+
+// Sending result to WWW. 
+	include 'Webserver_API_T_WWW.php';
 ?>
