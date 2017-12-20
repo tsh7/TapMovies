766b7cf1bcb65da9de219d1d25817e1c1119ddd5
kuldeep212 <33297628+kuldeep212@users.noreply.github.com>
2017-12-20 12:02:04 -0500

    Update search.php

--- API/search.php
+++ API/search.php
@@ -6,7 +6,7 @@
 $movie_name = $data["M_name"];
 
 //store the url of the search with the variable $movie_name
-$url = 'http://api.themoviedb.org/3/search/movie?api_key=4d9ca8e7d8e3381c21ffeb6f1306ee32&language=en-US&query='.$movie_name.'&page=1&include_adult=false';
+$url = 'http://api.themoviedb.org/3/search/movie?api_key=ec36674eb700de4ef91cc91d0fd2c966&language=en-US&query='.$movie_name.'&page=1&include_adult=false';
 
 //receive the data and store it to $json_data
 $json_data = file_get_contents($url);
