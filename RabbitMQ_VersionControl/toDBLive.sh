cb089c2b16bb1d1c462a411e266a8330bea5e27b
zethrath132 <bl233@njit.edu>
2017-12-20 18:10:27 -0500

    Create toDBLive.sh

---
+++ RabbitMQ_VersionControl/toDBLive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+        #sends the named Database Version to the Live Database server
+        scp ben@192.168.1.101:/home/ben/IT490/VersionControl/DatabaseVersion/$3 webuser@192.168.1.189:/home/DB
